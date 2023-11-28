ba654abff40d0702456d84401c2d06c6ef86afb4
BuiHuuCuong <buihuucuong2002@gmail.com>
2023-11-28 16:37:45 +0700

    chinh sua phan Login

---
+++ DoAnBanDienThoai/Models/Components/BrandMenu.cs
@@ -0,0 +1,20 @@
+﻿using DoAnBanDienThoai.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnBanDienThoai.Models.Components
+{
+    public class BrandMenu : ViewComponent
+    {
+        private readonly DoAnBanDienThoaiContext _context;
+
+        public BrandMenu(DoAnBanDienThoaiContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Brand.ToList());
+        }
+    }
+}
